57c4039af5dede5aee1f4f9b19723a3ebbde7c59
chenguanqiao <guanqiao.chen@ck-telecom.com>
2015-03-24 20:38:14 +0800

    Add Canon music script.

--- frequency-table.h
+++ frequency-table.h
@@ -1,28 +1,76 @@
 #ifndef  _FREQUENCY_TABLE_H__INC
 #define  _FREQUENCY_TABLE_H__INC
 
+#define FREQ_PITCH_LLC
+#define FREQ_PITCH_LLC_SHARP
+#define FREQ_PITCH_LLD_FLAT
+#define FREQ_PITCH_LLD
+#define FREQ_PITCH_LLD_SHARP
+#define FREQ_PITCH_LLE_FLAT
+#define FREQ_PITCH_LLE
+#define FREQ_PITCH_LLF
+#define FREQ_PITCH_LLF_SHARP
+#define FREQ_PITCH_LLG_FLAT
+#define FREQ_PITCH_LLG
+#define FREQ_PITCH_LLG_SHARP
+#define FREQ_PITCH_LLA_FLAT
+#define FREQ_PITCH_LLA
+#define FREQ_PITCH_LLA_SHARP
+#define FREQ_PITCH_LLB_FLAT
+#define FREQ_PITCH_LLB          (123.5)
+
 #define FREQ_PITCH_LC           (130.8)
+#define FREQ_PITCH_LC_SHARP     (138.6)
+#define FREQ_PITCH_LD_FLAT      (138.6)
 #define FREQ_PITCH_LD           (146.8)
+#define FREQ_PITCH_LD_SHARP     (155.5)
+#define FREQ_PITCH_LE_FLAT      (155.5)
 #define FREQ_PITCH_LE           (164.8)
 #define FREQ_PITCH_LF           (174.8)
+#define FREQ_PITCH_LF_SHARP     (185)
+#define FREQ_PITCH_LG_FLAT      (185)
 #define FREQ_PITCH_LG           (196)
+#define FREQ_PITCH_LG_SHARP     (207)
+#define FREQ_PITCH_LA_FLAT      (207)
 #define FREQ_PITCH_LA           (220)
+#define FREQ_PITCH_LA_SHARP     (233)
+#define FREQ_PITCH_LB_FLAT      (233)
 #define FREQ_PITCH_LB           (247)
 
 #define FREQ_PITCH_C            (261.5)
+#define FREQ_PITCH_C_SHARP      (288)
+#define FREQ_PITCH_D_FLAT       (288)
 #define FREQ_PITCH_D            (293.5)
+#define FREQ_PITCH_D_SHARP      (311)
+#define FREQ_PITCH_E_FLAT       (311)
 #define FREQ_PITCH_E            (329.5)
 #define FREQ_PITCH_F            (349)
+#define FREQ_PITCH_F_SHARP      (370)
+#define FREQ_PITCH_G_FLAT       (370)
 #define FREQ_PITCH_G            (392)
+#define FREQ_PITCH_G_SHARP      (415)
+#define FREQ_PITCH_A_FLAT       (415)
 #define FREQ_PITCH_A            (440)
+#define FREQ_PITCH_A_SHARP      (466)
+#define FREQ_PITCH_B_FLAT       (466)
 #define FREQ_PITCH_B            (494)
 
 #define FREQ_PITCH_HC           (523)
+#define FREQ_PITCH_HC_SHARP     (554)
+#define FREQ_PITCH_HD_FLAT      (554)
 #define FREQ_PITCH_HD           (587)
+#define FREQ_PITCH_HD_SHARP     (622)
+#define FREQ_PITCH_HE_FLAT      (622)
 #define FREQ_PITCH_HE           (659)
 #define FREQ_PITCH_HF           (698)
+#define FREQ_PITCH_HF_SHARP     (740)
+#define FREQ_PITCH_HG_FLAT      (740)
 #define FREQ_PITCH_HG           (784)
+#define FREQ_PITCH_HG_SHARP     (831)
+#define FREQ_PITCH_HA_FLAT      (831)
 #define FREQ_PITCH_HA           (880)
+#define FREQ_PITCH_HA_SHARP     (932)
+#define FREQ_PITCH_HB_FLAT      (932)
 #define FREQ_PITCH_HB           (998)
 
 #define FREQ_PITCH_HHC          (1047)
@@ -32,9 +80,6 @@
 #define FREQ_PITCH_HHG
 #define FREQ_PITCH_HHA
 #define FREQ_PITCH_HHB
-
-#define FREQ_PITCH_C_SHARP
-#define FREQ_PITCH_C_FLAT
 
 //extern int get_cpitch_freq(char pitch);
 
--- main.c
+++ main.c
@@ -46,7 +46,7 @@ void reset_keypress (void)
     return;
 }
 
-int get_cpitch_freq(char pitch, int level, int symbol)
+int get_cpitch_freq(char pitch, int level)
 {
     switch (pitch) {
         case 'A':
@@ -104,6 +104,134 @@ int get_cpitch_freq(char pitch, int level, int symbol)
                 return FREQ_PITCH_LG;
             else if (level == 1)
                 return FREQ_PITCH_HG;
+
+        default:
+            return FREQ_PITCH_C;
+    }
+}
+
+int get_cpitch_sharp_freq(char pitch, int level)
+{
+    switch (pitch) {
+        case 'A':
+            if (level == 0)
+                return FREQ_PITCH_A_SHARP;
+            else if (level == -1)
+                return FREQ_PITCH_LA_SHARP;
+            else if (level == 1)
+                return FREQ_PITCH_HA_SHARP;
+
+        case 'B':
+            if (level == 0)
+                return FREQ_PITCH_HC;
+            else if (level == -1)
+                return FREQ_PITCH_C;
+            else if (level == 1)
+                return FREQ_PITCH_HHC;
+
+        case 'C':
+            if (level == 0)
+                return FREQ_PITCH_C_SHARP;
+            else if (level == -1)
+                return FREQ_PITCH_LC_SHARP;
+            else if (level == 1)
+                return FREQ_PITCH_HC_SHARP;
+
+        case 'D':
+            if (level == 0)
+                return FREQ_PITCH_D_SHARP;
+            else if (level == -1)
+                return FREQ_PITCH_LD_SHARP;
+            else if (level == 1)
+                return FREQ_PITCH_HD_SHARP;
+            
+        case 'E':
+            if (level == 0)
+                return FREQ_PITCH_F;
+            else if (level == -1)
+                return FREQ_PITCH_LF;
+            else if (level == 1)
+                return FREQ_PITCH_HF;
+
+        case 'F':
+            if (level == 0)
+                return FREQ_PITCH_F_SHARP;
+            else if (level == -1)
+                return FREQ_PITCH_LF_SHARP;
+            else if (level == 1)
+                return FREQ_PITCH_HF_SHARP;
+
+        case 'G':
+            if (level == 0)
+                return FREQ_PITCH_G_SHARP;
+            else if (level == -1)
+                return FREQ_PITCH_LG_SHARP;
+            else if (level == 1)
+                return FREQ_PITCH_HG_SHARP;
+
+        default:
+            return FREQ_PITCH_C;
+    }
+}
+
+int get_cpitch_flat_freq(char pitch, int level)
+{
+    switch (pitch) {
+        case 'A':
+            if (level == 0)
+                return FREQ_PITCH_A_FLAT;
+            else if (level == -1)
+                return FREQ_PITCH_LA_FLAT;
+            else if (level == 1)
+                return FREQ_PITCH_HA_FLAT;
+
+        case 'B':
+            if (level == 0)
+                return FREQ_PITCH_B_FLAT;
+            else if (level == -1)
+                return FREQ_PITCH_LB_FLAT;
+            else if (level == 1)
+                return FREQ_PITCH_HB_FLAT;
+
+        case 'C':
+            if (level == 0)
+                return FREQ_PITCH_B;
+            else if (level == -1)
+                return FREQ_PITCH_LLB;
+            else if (level == 1)
+                return FREQ_PITCH_HB;
+
+        case 'D':
+            if (level == 0)
+                return FREQ_PITCH_D_FLAT;
+            else if (level == -1)
+                return FREQ_PITCH_LD_FLAT;
+            else if (level == 1)
+                return FREQ_PITCH_HD_FLAT;
+            
+        case 'E':
+            if (level == 0)
+                return FREQ_PITCH_E_FLAT;
+            else if (level == -1)
+                return FREQ_PITCH_LE_FLAT;
+            else if (level == 1)
+                return FREQ_PITCH_HE_FLAT;
+
+        case 'F':
+            if (level == 0)
+                return FREQ_PITCH_E;
+            else if (level == -1)
+                return FREQ_PITCH_LE;
+            else if (level == 1)
+                return FREQ_PITCH_HE;
+
+        case 'G':
+            if (level == 0)
+                return FREQ_PITCH_G_FLAT;
+            else if (level == -1)
+                return FREQ_PITCH_LG_FLAT;
+            else if (level == 1)
+                return FREQ_PITCH_HG_FLAT;
 
         default:
             return FREQ_PITCH_C;
@@ -886,14 +1014,24 @@ int main(int argc, char *argv[])
         err = read_music_file(&note);
         if (err == -1)
             break;
-        if (err != 4)
+        if (err != 4) {
+            printf("%d,", err);
+            printf("%c\n", note.pitch);
             continue;
+        }
 
-        freq = get_cpitch_freq(toupper((char)note.pitch), note.level, note.symbol);
+        if (note.symbol == 0) {
+            freq = get_cpitch_freq(toupper((char)note.pitch), note.level);
+        } else if (note.symbol == -1) {
+            freq = get_cpitch_flat_freq(toupper((char)note.pitch), note.level);
+        } else if (note.symbol == 1) {
+            freq = get_cpitch_sharp_freq(toupper((char)note.pitch), note.level);
+        }
 
-        freq = get_cpitch_freq(toupper((char)note.pitch), note.level, note.symbol);
+        //freq = get_cpitch_freq(toupper((char)note.pitch), note.level, note.symbol);
         delay_time = note.delay/2;
-        printf("input:%c\n", note.pitch);
+        printf("%c,", note.pitch);
+        fflush(stdout);
         //printf("input:%d\n", c);
 
         err = transfer_methods[method].transfer_loop(handle, samples, areas);
@@ -901,6 +1039,7 @@ int main(int argc, char *argv[])
             printf("Transfer failed: %s\n", snd_strerror(err));
     }
     close_music_file();
+    printf("\n");
 #else
     set_keypress();
     while (1) {
